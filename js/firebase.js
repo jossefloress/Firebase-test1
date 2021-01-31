4f3827f5e6b92afa1054fd2c751911286051c651
jossefloress <fj260996@gmail.com>
2021-01-31 01:41:27 -0600

    first commmit

---
+++ js/firebase.js
@@ -0,0 +1,20 @@
+var app_fireBase = {};
+
+(function () {
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    var firebaseConfig = {
+        apiKey: "AIzaSyCSx5AUxnFNgZqG31i4XosmRA_QTb6mV_s",
+        authDomain: "randomizer-draft.firebaseapp.com",
+        projectId: "randomizer-draft",
+        storageBucket: "randomizer-draft.appspot.com",
+        messagingSenderId: "295731651955",
+        appId: "1:295731651955:web:0b313072f09ba3649b9106",
+        measurementId: "G-LDWSEYP0DK"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    app_fireBase = firebase
+})()
+
